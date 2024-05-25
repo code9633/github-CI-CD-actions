80aa8e6648c01066eadd7b83419f73897ed4c7d8
code9633 <shashikaudara6@gmail.com>
2024-05-25 23:18:54 +0530

    new commit

--- src/main/java/com/example/ci_cd_pipeline/CiCdPipelineApplication.java
+++ src/main/java/com/example/ci_cd_pipeline/CiCdPipelineApplication.java
@@ -2,9 +2,25 @@ package com.example.ci_cd_pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CiCdPipelineApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Demo application of github CI/CD actions";
+	}
+
+//	echo "# github-ci-cd-actions" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/code9633/github-ci-cd-actions.git
+//	git push -u origin main
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdPipelineApplication.class, args);
